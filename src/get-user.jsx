bbb687870c65bb55078df4ba0fd02f5283c47fc1
Mohan Ramakrishnan <solar331@yahoo.ca>
2024-04-05 16:38:31 -0400

    Login completely fixed

--- src/get-user.jsx
+++ src/get-user.jsx
@@ -7,23 +7,32 @@ export function checkLogin (userName, passWord) {
 
     //Check for login 
     const newArray = userData.filter(element => element.userName == userName)
-    if (newArray[0].passWord==passWord) {
-        returnMessage = `Thank you ${newArray[0].firstName} for login as ${newArray[0].userType}`
+
+    console.log(newArray.length)
+
+    if ((newArray.length)) {
+        if (newArray[0].passWord==passWord) {
+            returnMessage = `Thank you ${newArray[0].firstName} for login as ${newArray[0].userType}`
+
+            //check for email format
+            if (!emailValidation(newArray[0].emailId)) {
+                returnMessage = returnMessage + "\n" + 'X -- Your email address format is wrong. please edit your email address in the profile' 
+            }
+
+            //check for phone format
+            if (!phoneValidation(newArray[0].phone)) {
+                returnMessage = returnMessage + "\n" + 'X -- Your phone format is wrong. please edit your phone number in the profile' 
+            }
+
+        }
+        else {
+            returnMessage = `Incorrect password`
+        }
     }
     else {
-        returnMessage = `Login failed`
+        returnMessage = 'Incorrect user name'
     }
 
-    //check for email format
-    if (!emailValidation(newArray[0].emailId)) {
-        returnMessage = returnMessage + "\n" + 'X -- Your email address format is wrong. please edit your email address in the profile' 
-    }
-
-     //check for phone format
-     if (!phoneValidation(newArray[0].phone)) {
-        returnMessage = returnMessage + "\n" + 'X -- Your phone format is wrong. please edit your phone number in the profile' 
-    }
-
-    //console.log("test 1")
     return returnMessage
+
 }
